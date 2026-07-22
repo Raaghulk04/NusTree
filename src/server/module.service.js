c1ed1427d82b59bef64440a042d59dbc2cf3365d
raaghul <raaghulk04@gmail.com>
2026-07-22 15:15:57 +0800

    perf: exclude description from global   module query to optimize page load speeds

--- src/server/module.service.js
+++ src/server/module.service.js
@@ -1,5 +1,16 @@
 import prisma from "@/lib/db";
 
 export async function getModules() {
-  return prisma.module.findMany();
+  return prisma.module.findMany({
+    select: {
+      id: true,
+      title: true,
+      department: true,
+      prereqTree: true,
+      preclusion: true,
+      prerequisite: true,
+      workload: true,
+      fulfillreqs: true,
+    },
+  });
 }
